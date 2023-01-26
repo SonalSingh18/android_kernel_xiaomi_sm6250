b3d441628187801da8a0e4d0ed6d8cb88d452401
akash07k <akash.galaxy07@gmail.com>
2023-01-26 13:22:05 +0530

    Integrate KernelSU hook

--- fs/exec.c
+++ fs/exec.c
@@ -1703,6 +1703,10 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
+// KernelSU hook
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			       void *envp, int *flags);
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1711,6 +1715,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
