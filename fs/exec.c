0310fb95d7fdf998c5478ad814cd9d87d8bd8ccf
Sonal Singh <sonal.singh.19993@gmail.com>
2023-02-20 21:42:12 +0530

    drivers: kernelsu: Upgrade KSU
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1705,10 +1705,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-// KernelSU hook
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
-
 /*
  * sys_execve() executes a new program.
  */
@@ -1717,7 +1713,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);  // call KSU hook first
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
